6032cf73ab6e363d0c5e34db5099c30766819606
BarbodH <habibi.k.barbod@gmail.com>
2025-06-21 12:01:18 -0400

    Apply prettier

--- website/_assets/js/history.js
+++ website/_assets/js/history.js
@@ -11,8 +11,7 @@
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
--- website/_steps/industry.md
+++ website/_steps/industry.md
@@ -6,5 +6,4 @@ enddate: 2023-10-11 20:30:00 -0500
 
 ![](https://cdn.discordapp.com/attachments/1108483433384124426/1137907257917583470/EVENTBRIGHT_BANNER_HARJOT_DRAFT_3.png)
 
-
 [Visit: Industry Night](https://yorkurobotics.com/industrynight)
